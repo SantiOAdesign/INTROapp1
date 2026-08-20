31eb27c8fa4071785d66f2eb49b9a6fab52f3e8c
SantiOAdesign <santiagoosorio2006@gmail.com>
2026-08-20 13:27:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ st.title("HOLAA!! MI NOMBRE ES SANTIAGO")
 st.header("En este espacio comienzo")
 image= Image.open('Charmander.jpg')
 st.image(image,caption='charmander')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
